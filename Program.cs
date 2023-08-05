02124b3b2d456e5157444425f3c25191391a44d2
Artem.Smirnov <artem-smirnov77@mail.ru>
2023-08-05 15:12:04 +0300

    2 задача

--- Program.cs
+++ Program.cs
@@ -2,8 +2,23 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-Console.Write("Введите число A: ");
-double numA = double.Parse(Console.ReadLine()!);
-Console.Write("Введите число B: ");
-double numB = double.Parse(Console.ReadLine()!);
-Console.WriteLine($"Результат возведения числа {numA} в степень {numB} будет {(long)Math.Pow(numA, numB):N0} ");
+// Console.Write("Введите число A: ");
+// double numA = double.Parse(Console.ReadLine()!);
+// Console.Write("Введите число B: ");
+// int numB = int.Parse(Console.ReadLine()!);
+// Console.WriteLine($"Результат возведения числа {numA} в степень {numB} будет {Math.Pow(numA, numB)} ");
+
+
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// Console.Write ("Введите число: ");
+// string array = Console.ReadLine()!;
+// int sum=0;
+// for (int i=0; i<array.Length;i++){
+//     sum += int.Parse(array[i].ToString());
+// };
+// Console.Write($"Сумма цифр в числе {String.Join("",array)} будет {sum}");
+
